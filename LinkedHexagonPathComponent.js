d4d1e2643b679bfd128a785de9b391bc5aeb86de
Anwesh43 <anweshthecool0@gmail.com>
2018-02-17 10:51:19 +0530

    adding Node class

--- LinkedHexagonPathComponent.js
+++ LinkedHexagonPathComponent.js
@@ -1,5 +1,5 @@
 const w = window.innerWidth, h = window.innerHeight, size = Math.min(w,h)/3
-class LinkedHeagonPathComponent extends HTMLElement {
+class LinkedHexagonPathComponent extends HTMLElement {
     constructor() {
         super()
         this.img = document.createElement('img')
@@ -16,5 +16,34 @@ class LinkedHeagonPathComponent extends HTMLElement {
     }
     connectedCallback() {
         this.render()
+    }
+}
+class Node {
+    constructor(i) {
+        this.i = i
+        const deg = i * Math.PI/3
+        this.x = (size/3)*Math.cos(deg)
+        this.y = (size/3)*Math.sin(deg)
+    }
+    draw(context) {
+        context.beginPath()
+        context.arc(this.x, this.y, 0, 2 * Math.PI)
+        context.fill()
+        if(this.neighbor) {
+            context.beinPath()
+            context.moveTo(this.x, this.y)
+            const x = this.neighbor.x, y = this.neighbor.y
+            context.lineTo(this.x + (x - this.x), this.y + (y - this.y))
+            context.stroke()
+        }
+    }
+    addNeighbor(neighbor) {
+        this.neighbor = neighbor
+    }
+    update(stobcb) {
+
+    }
+    startUpdating(startcb) {
+        
     }
 }
