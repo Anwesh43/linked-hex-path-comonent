058e7763c2da4af86e2ae1049d808b8836c33865
Anwesh43 <anweshthecool0@gmail.com>
2018-02-17 11:33:49 +0530

    adding Animator class

--- LinkedHexagonPathComponent.js
+++ LinkedHexagonPathComponent.js
@@ -119,3 +119,22 @@ class LinkedHexagonPath {
         this.curr.startUpdating()
     }
 }
+class Animator {
+    constructor() {
+        this.animated = false
+    }
+    start(updatecb) {
+        if(!this.animated) {
+            this.animated = true
+            this.interval = setInterval(() => {
+                updatecb()
+            },50)
+        }
+    }
+    stop() {
+        if(this.animated) {
+            this.animated = false
+            clearInterval(this.interval)
+        }
+    }
+}
